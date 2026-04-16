56566e5aeeb05d693922480c5330da497d3748d8
vish-git <vishwanathph@gmail.com>
2026-04-16 23:47:54 +0530

    puzzle tab update

--- src/pages/Puzzles.jsx
+++ src/pages/Puzzles.jsx
@@ -1,3 +1,32 @@
-export default function Games() {
-  return <h1>to do</h1>;
+import { useEffect, useState } from "react";
+import "./puzzles.css"
+
+function Puzzles() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("/data/puzzles.json")
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((err) => console.error("Fetch error:", err));
+  }, []);
+
+  if (!data) return <p>Loading...</p>;
+
+  return (
+    <div className="puzzle-card">
+      <h2>Puzzles</h2>
+
+      <div className="puzzle-items">
+        {data.map((item) => (
+          <div className="puzzle-item" key={item.id}>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
+
+export default Puzzles;
